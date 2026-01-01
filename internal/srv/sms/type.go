35977524dac1857ded0dd9037afd6b130e608d76
yite <coastq22889@icloud.com>
2026-01-01 16:38:51 +0800

    create key

--- internal/srv/sms/type.go
+++ internal/srv/sms/type.go
@@ -2,9 +2,11 @@ package sms
 
 type Secret struct {
 	AcctID     int
+	UserID     int
 	AcctName   string
 	Exchange   string
 	APIKey     string
 	APISecret  string
 	Passphrase string
+	MasterIs   int
 }
