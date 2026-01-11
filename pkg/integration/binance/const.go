6335f988314a7790d6449f38d7cefbd2a21347c2
yite <coastq22889@icloud.com>
2026-01-11 09:47:46 +0800

    const

---
+++ pkg/integration/binance/const.go
@@ -0,0 +1,14 @@
+package binance
+
+const (
+	BaseURL = "https://api.binance.com"
+
+	// Trading endpoints
+	EndpointOrder = "/api/v3/order"
+
+	// Account endpoints
+	EndpointAccount = "/api/v3/account"
+
+	// Market data endpoints
+	EndpointDepth = "/api/v3/depth"
+)
